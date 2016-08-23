31ac4b7336e5f9ee63ba6051d76352567aacd675
Cameron Davis <cameronjohndavis@gmail.com>
2016-08-23 17:21:40 +0100

    Real world two for one test

--- test/RealWorldTest.php
+++ test/RealWorldTest.php
@@ -5,11 +5,11 @@ namespace CodeClanShopping\Test;
 use CodeClanShopping\Basket;
 use CodeClanShopping\ShoppingItem;
 use CodeClanShopping\TotalPriceCalculator;
-use CodeClanShopping\Discounts\DiscountList;
 use CodeClanShopping\Discounts\PercentageDiscount;
 use CodeClanShopping\Discounts\LoyaltyDiscount;
 use CodeClanShopping\Discounts\TotalThresholdDiscount;
 use CodeClanShopping\Rounding;
+use CodeClanShopping\Discounts\TwoForOneDiscount;
 
 /**
  * This is not a unit test, rather it demonstrates the classes
@@ -29,6 +29,7 @@ class RealWorldTest extends \PHPUnit_Framework_TestCase
         // put some items in the basket
         $item1 = new ShoppingItem('PLU 1', 'Name 1', 1.09);
         $item2 = new ShoppingItem('PLU 2', 'Name 2', 2.77);
+        $item3 = new ShoppingItem('PLU 3', 'Name 3', 3.12);
         
         // add item 1
         $basket->addItem($item1);
@@ -53,30 +54,36 @@ class RealWorldTest extends \PHPUnit_Framework_TestCase
         $basket->addItem($item2);
         $basket->addItem($item2);
         $basket->addItem($item2);
+        
+        // add item 3, which is two-for-one
+        $basket->addItem($item3);
+        $basket->addItem($item3);
 
-        // caluclate the total before discounting
+        // calculate the total before discounting
         $calculator = new TotalPriceCalculator();
         $total = $calculator->calculateTotal($basket);
-        $this->assertEquals(33.06, $total);
-
-        // create a list of discounts
-        $discounts = new DiscountList();
+        $this->assertEquals(39.30, $total);
+        
+        // add a two-for-one discount which is first in line
+        $twoForOneDiscount = new TwoForOneDiscount(['PLU 3'], $basket);
+        $total1 = $twoForOneDiscount->applyDiscount($total);
+        $this->assertEquals(39.30 - 3.12, $total1);
         
         // add a total threshold discount for 10% after £20 total
         $percentageDiscount1 = new PercentageDiscount(0.1);
-        $discounts->append(new TotalThresholdDiscount(20.0, $percentageDiscount1));
+        $thresholdDiscount = new TotalThresholdDiscount(20.0, $percentageDiscount1);
+        $total2 = $thresholdDiscount->applyDiscount($total1);
+        $this->assertEquals($total1 * (1 - 0.1), $total2);
         
         // add a loyalty card discount 0f 2%
         $percentageDiscount2 = new PercentageDiscount(0.02);
-        $discounts->append(new LoyaltyDiscount($isLoyal, $percentageDiscount2));
-        
-        // apply the list of discounts to the total
-        $discountedTotal = $discounts->applyDiscount($total);
-        $this->assertEquals(29.15892, $discountedTotal);
+        $loyaltyDiscount = new LoyaltyDiscount($isLoyal, $percentageDiscount2);
+        $total3 = $loyaltyDiscount->applyDiscount($total2);
+        $this->assertEquals($total2 * (1 - 0.02), $total3);
         
         // round (read truncate) the final discounted price
         $rounding = new Rounding();
-        $roundedTotal = $rounding->roundPrice($discountedTotal);
-        $this->assertEquals(29.15, $roundedTotal);
+        $roundedTotal = $rounding->roundPrice($total3);
+        $this->assertEquals(31.91, $roundedTotal);
     }
 }
