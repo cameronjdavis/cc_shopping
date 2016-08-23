39c4dc1f3ed674eadedadb7c72fd4f6da0cfc8f5
Cameron Davis <cameronjohndavis@gmail.com>
2016-08-23 12:54:33 +0100

    Basket class and tests

--- src/ShoppingItem.php
+++ src/ShoppingItem.php
@@ -4,5 +4,43 @@ namespace CodeClanShopping;
 
 class ShoppingItem
 {
-    
+    /**
+     * Unique retailer ID of the item.
+     * @var string
+     */
+    private $plu;
+
+    /**
+     * Display name of the item.
+     * @var string
+     */
+    private $name;
+
+    /**
+     * Unit price of the item.
+     * @var float
+     */
+    private $price;
+
+    public function __construct($plu, $name, $price)
+    {
+        $this->plu = $plu;
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    public function getPlu()
+    {
+        return $this->plu;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
 }
